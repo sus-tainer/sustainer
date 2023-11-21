a68965473c912f5d72933aeb47cbc05dac4ba2d0
Nathaniel Murray <murrayn@hawaii.edu>
2023-11-21 12:20:53 -1000

    Vendor Collection

---
+++ app/imports/api/vendor/Vendors.js
@@ -0,0 +1,33 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The Vendors. It encapsulates state and variable values for stuff.
+ */
+class Vendors {
+  constructor() {
+    // The name of this collection.
+    this.name = 'Vendors';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      vendorName: String,
+      firstName: String,
+      lastName: String,
+      email: String,
+      password: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the Vendors.
+ * @type {Vendors}
+ */
+export const VendorOrder = new Vendors();
---
+++ app/imports/ui/pages/VendorSignUp.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link, Navigate } from 'react-router-dom';
+import { Accounts } from 'meteor/accounts-base';
+import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
+import SimpleSchema from 'simpl-schema';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
+
+const VendorSignUp = ({ location }) => {
+  const [error, setError] = useState('');
+  const [redirectToReferer, setRedirectToRef] = useState(false);
+
+  const schema = new SimpleSchema({
+    vendorName: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: {
+      type: String,
+    },
+  });
+  const bridge = new SimpleSchema2Bridge(schema);
+
+  const submit = (doc) => {
+    const { email, password } = doc;
+    Accounts.createUser({ email, username: email, password }, (err) => {
+      if (err) {
+        setError(err.reason);
+      } else {
+        setError('');
+        setRedirectToRef(true);
+      }
+    });
+  };
+
+  const { from } = location?.state || { from: { pathname: '/add' } };
+  if (redirectToReferer) {
+    return <Navigate to={from} />;
+  }
+
+  return (
+    <Container id="signup-page" className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={12} md={10} lg={8}>
+          <Col className="text-center">
+            <h2>Register your vendor account</h2>
+          </Col>
+          <AutoForm schema={bridge} onSubmit={(data) => submit(data)}>
+            <Card>
+              <Card.Body>
+                <TextField name="vendorName" placeholder="Vendor Name" inputClassName="form-control form-control-lg mb-3" />
+                <TextField name="firstName" placeholder="First Name" inputClassName="form-control form-control-lg mb-3" />
+                <TextField name="lastName" placeholder="Last Name" inputClassName="form-control form-control-lg mb-3" />
+                <TextField name="email" placeholder="E-mail address" inputClassName="form-control form-control-lg mb-3" />
+                <TextField name="password" placeholder="Password" type="password" inputClassName="form-control form-control-lg mb-3" />
+                <ErrorsField />
+                <SubmitField />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+          <Alert variant="light">
+            Already have an account? Login <Link to="/signin">here</Link>
+          </Alert>
+          {error === '' ? (
+            ''
+          ) : (
+            <Alert variant="danger">
+              <Alert.Heading>Registration was not successful</Alert.Heading>
+              {error}
+            </Alert>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+VendorSignUp.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.string,
+  }),
+};
+
+VendorSignUp.defaultProps = {
+  location: { state: '' },
+};
+
+export default VendorSignUp;
