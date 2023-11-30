70ccc946538dbfdc37d79ee6f58907a93400de1f
Nathaniel Murray <murrayn@hawaii.edu>
2023-11-30 13:09:31 -1000

    Fixed ready check routing

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -48,9 +48,9 @@ const App = () => {
           <Route path="/list" element={<ProtectedRoute><ListStuff /></ProtectedRoute>} />
           <Route path="/add" element={<ProtectedRoute><AddContainer /></ProtectedRoute>} />
           <Route path="/qrcode" element={<ProtectedRoute><QRCodeGenerator /></ProtectedRoute>} />
-          <Route path="/vendororder" element={<VendorProtectedRoute><AddVendorOrder /></VendorProtectedRoute>} />
-          <Route path="/listvendororder" element={<VendorProtectedRoute><ListVendorOrder /></VendorProtectedRoute>} />
-          <Route path="/edit/:_id" element={<VendorProtectedRoute><EditVendorOrder /></VendorProtectedRoute>} />
+          <Route path="/vendororder" element={<VendorProtectedRoute ready={ready}><AddVendorOrder /></VendorProtectedRoute>} />
+          <Route path="/listvendororder" element={<VendorProtectedRoute> ready={ready}<ListVendorOrder /></VendorProtectedRoute>} />
+          <Route path="/edit/:_id" element={<VendorProtectedRoute ready={ready}><EditVendorOrder /></VendorProtectedRoute>} />
           <Route path="/admin-list" element={<AdminProtectedRoute ready={ready}><ListContainersAdmin /></AdminProtectedRoute>} />
           <Route path="/add-container" element={<AdminProtectedRoute ready={ready}><AddContainer /></AdminProtectedRoute>} />
           <Route path="/scan" element={<AdminProtectedRoute ready={ready}><QRCodeScanner /></AdminProtectedRoute>} />
