a0da5396fbd8a340899013532a6d4ab2788f17b3
thomasarivera <thomas5info@gmail.com>
2023-12-01 09:18:50 -1000

    Added button that leads to home

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
   Meteor.logout();
+
   return (
-    <Col id="signout-page" className="text-center py-3"><h2>You are signed out.</h2></Col>
+    <Container className="py-3">
+      <Row>
+        <Col id="signout-page" className="text-center py-3">
+          <h2>You are signed out.</h2>
+        </Col>
+      </Row>
+
+      <Row className="text-center py-3">
+        <Link to="/">
+          <Button type="submit" variant="primary">Home</Button>
+        </Link>
+      </Row>
+    </Container>
   );
 };
 
