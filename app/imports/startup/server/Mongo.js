a38e80dcb6c64a8f3f59bb5e0da956d151f9dc08
thomasarivera <thomas5info@gmail.com>
2023-12-07 20:40:43 -1000

    Removed all mentions to "stuff" collections

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -3,22 +3,6 @@ import { Containers } from '../../api/container/Containers.js';
 import { VendorOrder } from '../../api/vendor/VendorOrder';
 import { Vendors } from '../../api/vendor/Vendors';
 
-/* eslint-disable no-console */
-
-// // Initialize the database with a default data document.
-// const addData = (data) => {
-//   console.log(`  Adding: ${data.name} (${data.owner})`);
-//   Stuffs.collection.insert(data);
-// };
-//
-// // Initialize the StuffsCollection if empty.
-// if (Stuffs.collection.find().count() === 0) {
-//   if (Meteor.settings.defaultData) {
-//     console.log('Creating default data.');
-//     Meteor.settings.defaultData.forEach(data => addData(data));
-//   }
-// }
-
 // Initialize the database with a default data document.
 const addContainer = (data) => {
   console.log(`  Adding: ${data.owner} (${data.size})`);
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,20 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { Stuffs } from '../../api/stuff/Stuff';
 import { Containers } from '../../api/container/Containers';
 import { VendorOrder } from '../../api/vendor/VendorOrder';
 import { Vendors } from '../../api/vendor/Vendors';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
-Meteor.publish(Stuffs.userPublicationName, function () {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Stuffs.collection.find({ owner: username });
-  }
-  return this.ready();
-});
-
 Meteor.publish(Containers.userPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'vendor')) {
     const user = Meteor.users.findOne(this.userId);
@@ -43,13 +34,6 @@ Meteor.publish(Vendors.userPublicationName, function () {
 
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise, publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
-  }
-  return this.ready();
-});
-
 Meteor.publish(Containers.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Containers.collection.find();
