92b0aa77d3e1c6f6f74a12d4b63ddcd71053f297
thomasarivera <thomas5info@gmail.com>
2023-12-10 11:00:42 -1000

    Added images to about page

--- app/imports/ui/pages/About.jsx
+++ app/imports/ui/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card, Col, Row } from 'react-bootstrap';
+import { Container, Card, Col, Row, Image } from 'react-bootstrap';
 
 const About = () => (
   <Container className="justify-content-center text-center p-3">
@@ -12,6 +12,40 @@ const About = () => (
               Single-use takeout containers and food wrappers make up a significant portion—up to one-third—of the debris discovered along Hawai’i&apos;s shorelines. Sustainer is dedicated to tackling this environmental challenge by
               providing a convenient and user-friendly reusable takeout container system. Our aim is to contribute to reducing this prevalent waste and elimi nating debris along Hawai’i&apos;s coastal areas. Join us in making a positive
               impact by adopting our sustainable solution for takeout packaging.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+    <Row className="align-content-center">
+      <Col>
+        <Image src="./images/small-container.png" fluid />
+      </Col>
+      <Col>
+        <Image src="./images/large-container.png" fluid />
+      </Col>
+      <Col>
+        <Image src="./images/medium-container.png" fluid />
+      </Col>
+    </Row>
+    <h1>Our Mission</h1>
+    <Row xs={1} sm={2} className="justify-content-center mb-5">
+      <Col xs="auto" sm="auto" md="auto" lg="auto">
+        <Card className="mission-card" style={{ width: '14em' }}>
+          <Card.Body>
+            <Card.Title>Reduce</Card.Title>
+            <Card.Text>
+              Minimize container loss and enhance the successful implementation of the <a href="https://www.fullcycletakeouthawaii.org/" target="/blank">Full Cycle Takeout program</a>.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs="auto" sm="auto" md="auto" lg="auto">
+        <Card className="mission-card" style={{ width: '14em' }}>
+          <Card.Body>
+            <Card.Title>Reuse</Card.Title>
+            <Card.Text>
+              Establish a streamlined system to guarantee the return of all containers, facilitating their reuse and sustainability.
             </Card.Text>
           </Card.Body>
         </Card>
