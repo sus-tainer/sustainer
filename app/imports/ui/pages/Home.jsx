8449c4f93c93ed35bc54ef659f537bd17d2231e8
Aaron Ancheta <al.ancheta10@gmail.com>
2023-12-11 15:52:09 -1000

    Clean up

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Roles } from 'meteor/alanning:roles';
 import FooterHome from '../components/FooterHome';
+import PieChartStats from '../components/PieChartStats';
 
 /* A simple static component to render some text for the landing page. */
 const Home = () => {
@@ -19,12 +20,12 @@ const Home = () => {
             <Row className="justify-content-center align-items-center">
               <Col xs="auto" className="my-3">
                 <Button className="rounded-0" variant="outline-light-landing" size="lg" href="/qrcode">
-                    Generate QR Code
+                  Generate QR Code
                 </Button>
               </Col>
               <Col xs="auto">
                 <Button className="rounded-0" variant="light" size="lg" href="/payment">
-                    Choose Payment Method
+                  Choose Payment Method
                 </Button>
               </Col>
             </Row>
@@ -34,46 +35,48 @@ const Home = () => {
       {/* <------------------ End of User Homepage --------------------> */}
       {/* <------------------ Admin Homepage --------------------> */}
       {Roles.userIsInRole(Meteor.userId(), 'admin') ? ([
-        <div className="landing-not-user">
-          <Container className="justify-content-center">
-            <Row className="">
-              <Col className="px-0">
-                <Button className="rounded-4 " variant="outline-light" size="lg" href="/add-container">
-                  Add Containers
-                </Button>
-              </Col>
-              <Col className="px-0">
-                <Button className="rounded-4" variant="outline-light" size="lg" href="/scan-container">
-                  Return Containers
-                </Button>
-              </Col>
-              <Col className="px-0">
-                <Button className="rounded-4" variant="outline-light" size="lg" href="/admin-list">
-                  Containers List
-                </Button>
-              </Col>
-              <Col className="px-0">
-                <Button className="rounded-4" variant="outline-light" size="lg" href="/charge-user">
-                  Charge User
-                </Button>
-              </Col>
-            </Row>
-          </Container>
+        <div className="landing-admin adminhome-column">
+          <div className="adminhome-piechart">
+            <h2>Container Retention Rate</h2>
+            <PieChartStats />
+          </div>
+          <div>
+            <Container className="justify-content-center">
+              <Row className="me-3">
+                <Col className="px-0">
+                  <Button className="rounded-0" variant="outline-light" size="lg" href="/add-container">
+                    Add Containers
+                  </Button>
+                  <Button className="rounded-0" variant="outline-light" size="lg" href="/scan-container">
+                    Return Containers
+                  </Button>
+                </Col>
+                <Col className="px-0">
+                  <Button className="rounded-0" variant="outline-light" size="lg" href="/admin-list">
+                    Containers List
+                  </Button>
+                  <Button className="rounded-0" variant="outline-light" size="lg" href="/charge-user">
+                    Charge User
+                  </Button>
+                </Col>
+              </Row>
+            </Container>
+          </div>
         </div>,
       ]) : ''}
       {/* <------------------ End of Admin Homepage --------------------> */}
       {/* <------------------ Vendor Homepage --------------------> */}
       {Roles.userIsInRole(Meteor.userId(), 'vendor') ? ([
-        <div className="landing-not-user">
+        <div className="landing-vendor">
           <Container>
             <Row className="justify-content-center align-items-center">
               <Col xs="auto" className="my-3">
-                <Button className="rounded-0" variant="outline-light-landing" size="lg" href="/listvendororder">
+                <Button className="rounded-0" variant="outline-light" size="lg" href="/listvendororder">
                   Vendor Order List
                 </Button>
               </Col>
               <Col xs="auto">
-                <Button className="rounded-0" variant="light" size="lg" href="/vendororder">
+                <Button className="rounded-0" variant="outline-light" size="lg" href="/vendororder">
                   Vendor Order Form
                 </Button>
               </Col>
